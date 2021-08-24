fdf2473ec017fe25a40d5f0c35ef7a5b954e0691
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-08-24 18:56:35 +0100

    - SquarePuzzleBoard.move() implemented and tested - removed direction in move tuples as these are redundant

--- board_cls.py
+++ board_cls.py
@@ -5,7 +5,9 @@
 import numpy as np
 
 class SquarePuzzleBoard:
-
+    """
+    Note that 0 is used to indicate the blank square
+    """
 
     def __init__(self, n):
         """
@@ -21,6 +23,7 @@ class SquarePuzzleBoard:
     def create_solution_board(self) -> np.array:
         return np.array([i for i in range(1, self.n**2)] + [0]).reshape((self.n,-1))
 
+
     def create_starting_board(self) -> np.array:
         """
         Randomly initialise a solvable board. The initialisation is done through random moves from a solved board to
@@ -33,52 +36,69 @@ class SquarePuzzleBoard:
         # TODO: randomly shuffle
         return square_positions
 
-    def move(self, square, move):
-        pass
+
+    def move(self, move_tuple):
+        if move_tuple in self.valid_moves():
+            row = move_tuple[0]
+            col = move_tuple[1]
+            number_moved = self.square_positions[row,col]
+            self.square_positions[self.square_positions == 0] = number_moved
+            self.square_positions[row, col] = 0
+
+            self.present_board()
+
+        else:
+            print('Invalid move') #TODO: check this isn't repeated
+
 
     def check_move_valid(self):
         pass
 
+
     def valid_moves(self) -> list:
         """
-        Returns a list of valis moves, each move a tuple of format (row, column, direction)
+        Returns a list of valis moves, each move a tuple of format (row, column)
         :return valid_moves: a list of valid moves
         """
         empty_row, empty_col = [i[0] for i in np.where(self.square_positions == 0)]
         valid_moves = []
         if empty_row > 0:
-            down_move = (empty_row - 1, empty_col, 'd')
+            down_move = (empty_row - 1, empty_col)
             valid_moves.append(down_move)
         if empty_row < self.n - 1:
-            up_move = (empty_row + 1, empty_col, 'u')
+            up_move = (empty_row + 1, empty_col)
             valid_moves.append(up_move)
         if empty_col > 0:
-            right_move = (empty_row, empty_col - 1, 'r')
+            right_move = (empty_row, empty_col - 1)
             valid_moves.append(right_move)
         if empty_col < self.n - 1:
-            left_move = (empty_row, empty_col + 1, 'l')
+            left_move = (empty_row, empty_col + 1)
             valid_moves.append(left_move)
 
         return valid_moves
 
+
     def check_board_solved(self) -> bool:
 
         return np.all(self.square_positions == self.solution_board)
+
 
     def present_board(self):
         board_str = ''
         board_str += ' ' + (self.max_char_len + 3) * self.n * '-' + '-\n'
         for i in range(self.n):
             for j in range(self.n):
-                board_str += ' | ' + str(self.square_positions[i,j]).rjust(self.max_char_len)
+                board_str += ' | ' + (str(self.square_positions[i,j]).rjust(self.max_char_len) if self.square_positions[i,j] != 0 else self.max_char_len * ' ')
             board_str += ' |\n'
             board_str += ' ' + (self.max_char_len + 3)* self.n * '-' + '-\n'
         print(board_str)
+
 
     def move_input_checker(self):
         pass
 
 if __name__ == '__main__':
+    # TODO: docstring everything
     board = SquarePuzzleBoard(4)
     print(board.square_positions)
     print(board.valid_moves())
--- tests/test_baord_cls.py
+++ tests/test_baord_cls.py
@@ -10,14 +10,14 @@ class TestSquarePuzzleBoard(unittest.TestCase):
         n = 4
         board = SquarePuzzleBoard(n)
         board.square_positions = np.array([i for i in range(1, n + 1)] + [0] + [i for i in range(n + 1, n**2)]).reshape((n, -1))
-        self.assertEqual(board.valid_moves(), [(0, 0, 'd'), (2, 0, 'u'), (1, 1, 'l')])
+        self.assertEqual(board.valid_moves(), [(0, 0), (2, 0), (1, 1)])
 
         board.square_positions = np.array([i for i in range(1, n)] + [0] + [i for i in range(n, n ** 2)]).reshape((n, -1))
-        self.assertEqual(board.valid_moves(), [(1, 3, 'u'), (0, 2, 'r')])
+        self.assertEqual(board.valid_moves(), [(1, 3), (0, 2)])
 
         board.square_positions = np.array([i for i in range(1, n+2)] + [0] + [i for i in range(n+2, n ** 2)]).reshape(
             (n, -1))
-        self.assertEqual(board.valid_moves(), [(0, 1, 'd'), (2, 1, 'u'), (1, 0, 'r'), (1, 2, 'l')])
+        self.assertEqual(board.valid_moves(), [(0, 1), (2, 1), (1, 0), (1, 2)])
 
     def test_create_solution_board(self):
         for n in [2,3,4,5,6,10,178]:
@@ -38,6 +38,16 @@ class TestSquarePuzzleBoard(unittest.TestCase):
             board = SquarePuzzleBoard(n)
             board.square_positions = not_solution
             self.assertFalse(board.check_board_solved())
+
+    def test_move_valid_move(self):
+        n = 4
+        board = SquarePuzzleBoard(n)
+        board.square_positions = np.array([[ 1, 2, 3, 4], [ 5, 6, 7, 8], [ 9, 10, 11, 12], [13, 14, 15, 0]])
+        board.move((n-2,n-1))
+        current_board = np.array([[ 1, 2, 3, 4], [ 5, 6, 7, 8], [ 9, 10, 11, 0], [13, 14, 15, 12]])
+        print(board.square_positions)
+        board.move((n - 2, n - 1))
+        print(board.square_positions)
 
 
 if __name__ == '__main__':
