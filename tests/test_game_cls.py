d50fef660fddcad00883f4b64d0debcffb38bce3
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-08-25 09:53:56 +0100

    Created test_input_parser_cls.py and implemented testing for PuzzleGame class

---
+++ tests/test_game_cls.py
@@ -0,0 +1,18 @@
+"""
+    Author: Jamie Bamforth
+    Tests code in game_cls.py
+"""
+
+import unittest
+
+
+# TODO: comment all this
+
+
+class TestPuzzleGame(unittest.TestCase):
+    def test_init(self):
+        pass  # TODO: implement
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_input_parser_cls.py
+++ tests/test_input_parser_cls.py
@@ -1,17 +1,21 @@
 """
     Author: Jamie Bamforth
-    Tests code in board_cls.py
+    Tests code in board_cls.py and the create_game() function in main
 """
 
 import unittest
-
-
-# TODO: comment all this
+from main import create_game
+from board_cls import SquarePuzzleBoard
+from input_parser_cls import InputParser
+from game_cls import PuzzleGame
 
 
 class TestInputParser(unittest.TestCase):
     def test_init(self):
-        pass  # TODO: implement
+        game = create_game()
+        self.assertTrue(isinstance(game, PuzzleGame))
+        self.assertTrue(isinstance(game.board, SquarePuzzleBoard))
+        self.assertTrue(isinstance(game.parser, InputParser))
 
 
 if __name__ == "__main__":
