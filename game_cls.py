01c1d5851b7a29d82bcb54d1e6c3545ef9906203
Jamie Bamforth <jamie_bamforth@hotmail.co.uk>
2021-08-25 09:46:37 +0100

    Commented PuzzleGame class

--- game_cls.py
+++ game_cls.py
@@ -6,11 +6,20 @@
 
 class PuzzleGame:
     def __init__(self, board, parser):
+        """
+        Initialise a puzzle game with the relevant board and input parser
+        :param board: an instance of the relevant puzzle board
+        :param parser: an instance of the parser that interacts with the users input
+        """
         self.board = board
         self.parser = parser
 
     def intro_message(self):
-        print("Welcome to the Puzzle15 board challenge!\n")
+        """
+        Prints an intro message and instructions for the user
+        :return:
+        """
+        print(f"Welcome to the Puzzle{self.board.board_side_length**2 - 1} board challenge!\n")
         print(
             f"You will see a board below with {self.board.board_side_length**2} squares containing numbers 1 to {self.board.board_side_length**2 - 1} and an empty square, all randomly mixed."
         )
@@ -21,6 +30,11 @@ class PuzzleGame:
         print("#####################################\n")
 
     def play_game(self):
+        """
+        Allows user to input moves and checks updated board against the solution.
+        When solution is reached, prints a congratulatory message and finishes game
+        :return:
+        """
         while not self.board.check_board_solved():
             self.board.present_board()
             move = self.parser.get_move_tuple_from_user()
@@ -28,6 +42,10 @@ class PuzzleGame:
         self._solution_message()
 
     def _solution_message(self):
+        """
+        Prints a congratulatory message
+        :return:
+        """
         print("\n\n****************** Well Done! ******************\n")
         self.board.present_board()
         print("\n************************************************")
